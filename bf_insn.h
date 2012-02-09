a07f2a0b0840bca3a0c74d27bdd72d37471122a3
michaelwskwan <michael.kwan08@imperial.ac.uk>
2012-02-09 21:50:13 +0000

    Simplified bf_insn by each instruction to hold the head of the list instead of holding a node containing the head.

--- bf_insn.h
+++ bf_insn.h
@@ -11,18 +11,40 @@ typedef struct bf_insn_part {
 
 /*
  * Our abstraction of an instruction. libopcodes discards instruction
- * information so this is our way of storing it.
+ * information so this is our way of storing it. A bf_insn consists of a list
+ * of parts which hold the mnemonic and operands.
  */
 typedef struct bf_insn {
 	/*
-	 * Holds the list of parts for the instruction
+	 * Holds the address of the instruction.
 	 */
-	bf_insn_part part_list;
+	bfd_vma		 vma;
+
+	/*
+	 * Start of linked list of parts.
+	 */
+	struct list_head part_list;
 } bf_insn;
 
-extern bf_insn * init_bf_insn(void);
+/*
+ * Returns a bf_insn object. close_bf_insn must be called to allow the object
+ * to properly clean up.
+ */
+extern bf_insn * init_bf_insn(bfd_vma);
+
+/*
+ * Adds to the tail of the part list.
+ */
 extern void add_insn_part(bf_insn *, char *);
+
+/*
+ * Prints the bf_insn to stdout.
+ */
 extern void print_bf_insn(bf_insn *);
+
+/*
+ * Closes a bf_insn obtained from calling init_bf_insn.
+ */
 extern void close_bf_insn(bf_insn *);
 
 #endif
