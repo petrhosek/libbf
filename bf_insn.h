29b027ac946b3b918f41e0b9bdaed2887d976a7a
michaelwskwan <michael.kwan08@imperial.ac.uk>
2012-02-11 17:10:55 +0000

    Added struct to prefix binary_file structures which we are no longer typedef'ing.

--- bf_insn.h
+++ bf_insn.h
@@ -4,16 +4,16 @@
 #include "binary_file.h"
 #include "include/list.h"
 
-typedef struct bf_insn_part {
+struct bf_insn_part {
 	struct list_head list;
 	char *		 str;
-} bf_insn_part;
+};
 
 /*
  * Our abstraction of an instruction. A bf_insn consists of a list of parts
  * which hold the mnemonic and operands.
  */
-typedef struct bf_insn {
+struct bf_insn {
 	/*
 	 * Holds the address of the instruction.
 	 */
@@ -23,27 +23,27 @@ typedef struct bf_insn {
 	 * Start of linked list of parts.
 	 */
 	struct list_head part_list;
-} bf_insn;
+};
 
 /*
  * Returns a bf_insn object. close_bf_insn must be called to allow the object
  * to properly clean up.
  */
-extern bf_insn * init_bf_insn(bfd_vma);
+extern struct bf_insn * init_bf_insn(bfd_vma);
 
 /*
  * Adds to the tail of the part list.
  */
-extern void add_insn_part(bf_insn *, char *);
+extern void add_insn_part(struct bf_insn *, char *);
 
 /*
  * Prints the bf_insn to stdout.
  */
-extern void print_bf_insn(bf_insn *);
+extern void print_bf_insn(struct bf_insn *);
 
 /*
  * Closes a bf_insn obtained from calling init_bf_insn.
  */
-extern void close_bf_insn(bf_insn *);
+extern void close_bf_insn(struct bf_insn *);
 
 #endif
