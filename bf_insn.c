e0435913ca1ec8ecae906a432e904a13d6c8bb9c
michaelwskwan <michael.kwan08@imperial.ac.uk>
2012-02-11 17:10:39 +0000

    Added struct to prefix binary_file structures which we are no longer typedef'ing.

--- bf_insn.c
+++ bf_insn.c
@@ -1,27 +1,27 @@
 #include "bf_insn.h"
 
-bf_insn * init_bf_insn(bfd_vma vma)
+struct bf_insn * init_bf_insn(bfd_vma vma)
 {
-	bf_insn * insn = xmalloc(sizeof(bf_insn));
-	insn->vma      = vma;
+	struct bf_insn * insn = xmalloc(sizeof(struct bf_insn));
+	insn->vma	      = vma;
 
 	INIT_LIST_HEAD(&insn->part_list);
 	return insn;
 }
 
-void add_insn_part(bf_insn * insn, char * str)
+void add_insn_part(struct bf_insn * insn, char * str)
 {
-	bf_insn_part * part = xmalloc(sizeof(bf_insn_part));
-	part->str	    = xstrdup(str);
+	struct bf_insn_part * part = xmalloc(sizeof(struct bf_insn_part));
+	part->str		   = xstrdup(str);
 
 	INIT_LIST_HEAD(&part->list);
 	list_add_tail(&part->list, &insn->part_list);
 }
 
-void print_bf_insn(bf_insn * insn)
+void print_bf_insn(struct bf_insn * insn)
 {
 	if(insn != NULL) {
-		bf_insn_part * pos;
+		struct bf_insn_part * pos;
 
 		list_for_each_entry(pos, &insn->part_list, list) {
 			printf("%s", pos->str);
@@ -29,11 +29,11 @@ void print_bf_insn(bf_insn * insn)
 	}
 }
 
-void close_bf_insn(bf_insn * insn)
+void close_bf_insn(struct bf_insn * insn)
 {
 	if(insn != NULL) {
-		bf_insn_part * pos;
-		bf_insn_part * n;
+		struct bf_insn_part * pos;
+		struct bf_insn_part * n;
 
 		list_for_each_entry_safe(pos, n, &insn->part_list, list) {
 			list_del(&pos->list);
