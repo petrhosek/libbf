3a7aaae5945dd54a65ccc64564c21c43ff7ab15f
Mike Kwan <mk08@doc.ic.ac.uk>
2012-02-13 15:59:08 +0000

    Changed function names to be bf_*

--- bf_basic_blk.h
+++ bf_basic_blk.h
@@ -17,7 +17,7 @@ struct bf_basic_blk_part {
  */
 struct bf_basic_blk {
 	/*
-	 * Holds that the basic block starts at.
+	 * Holds the VMA that the basic block starts at.
 	 */
 	bfd_vma		      vma;
 
@@ -51,50 +51,57 @@ struct bf_basic_blk {
 typedef struct bf_basic_blk * BF_BASIC_BLK_PTR;
 
 /*
- * Returns a bf_basic_blk object. close_bf_basic_blk must be called to allow
+ * Returns a bf_basic_blk object. bf_close_basic_blk must be called to allow
  * the object to properly clean up.
  */
-extern struct bf_basic_blk * init_bf_basic_blk(struct binary_file *, bfd_vma);
+extern struct bf_basic_blk * bf_init_basic_blk(struct binary_file *, bfd_vma);
+
+/*
+ * Splits a bf_basic_blk at the specified VMA. Returns the new bf_basic_blk.
+ */
+extern struct bf_basic_blk * bf_split_blk(struct binary_file *,
+		struct bf_basic_blk *, bfd_vma);
 
 /*
  * Creates a link between the from the first basic block to the second one.
  */
-extern void bf_add_next_basic_blk(struct bf_basic_blk *, struct bf_basic_blk *);
+extern void bf_add_next_basic_blk(struct bf_basic_blk *,
+		struct bf_basic_blk *);
 
 /*
  * Adds to the tail of the instruction list.
  */
-extern void add_insn(struct bf_basic_blk *, struct bf_insn *);
+extern void bf_add_insn_to_bb(struct bf_basic_blk *, struct bf_insn *);
 
 /*
  * Prints the bf_basic_blk to stdout.
  */
-extern void print_bf_basic_blk(struct bf_basic_blk *);
+extern void bf_print_basic_blk(struct bf_basic_blk *);
 
 /*
  * Prints the bf_basic_blk to a FILE.
  */
-extern void print_bf_basic_blk_dot(FILE *, struct bf_basic_blk *);
+extern void bf_print_basic_blk_dot(FILE *, struct bf_basic_blk *);
 
 /*
- * Closes a bf_basic_blk obtained from calling init_bf_basic_blk. This will
- * also call close_bf_insn for each bf_insn contained in the basic block.
+ * Closes a bf_basic_blk obtained from calling bf_init_basic_blk. This will
+ * not call bf_close_insn for each bf_insn contained in the basic block.
  */
-extern void close_bf_basic_blk(struct bf_basic_blk *);
+extern void bf_close_basic_blk(struct bf_basic_blk *);
 
 /*
  * Adds a basic block to the bb_table of binary_file.
  */
-extern void add_bb(struct binary_file *, struct bf_basic_blk *);
+extern void bf_add_bb(struct binary_file *, struct bf_basic_blk *);
 
 /*
  * Gets basic block for the starting VMA.
  */
-extern struct bf_basic_blk * get_bb(struct binary_file *, bfd_vma);
+extern struct bf_basic_blk * bf_get_bb(struct binary_file *, bfd_vma);
 
 /*
  * Checks for the existence of a basic block in the bb_table of binary_file.
  */
-extern bool exists_bb(struct binary_file *, bfd_vma);
+extern bool bf_exists_bb(struct binary_file *, bfd_vma);
 
 #endif
