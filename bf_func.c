237686fc71076f306dec197279e64cbca31a8afd
Mike Kwan <mk08@doc.ic.ac.uk>
2012-02-21 16:12:48 +0000

    Added symbol information to function.

--- bf_func.c
+++ bf_func.c
@@ -1,11 +1,13 @@
 #include "bf_func.h"
+#include "bf_sym_tab.h"
 
-struct bf_func * bf_init_func(struct bf_basic_blk * bb, bfd_vma vma)
+struct bf_func * bf_init_func(struct binary_file * bf,
+		struct bf_basic_blk * bb, bfd_vma vma)
 {
 	struct bf_func * func = xmalloc(sizeof(struct bf_func));
 	func->bb	      = bb;
 	func->vma	      = vma;
-
+	func->sym	      = bf_get_sym(bf, vma);
 	return func;
 }
 
@@ -43,5 +45,16 @@ void bf_close_func_table(struct binary_file * bf)
 	htable_for_each_entry_safe(func, cur_entry, n, &bf->func_table, entry) {
 		htable_del_entry(&bf->func_table, cur_entry);
 		bf_close_func(func);
+	}
+}
+
+void bf_for_each_func(struct binary_file * bf,
+		void (*handler)(struct binary_file * bf, struct bf_func *))
+{
+	struct htable_entry * cur_entry;
+	struct bf_func *      func;
+
+	htable_for_each_entry(func, cur_entry, &bf->func_table, entry) {
+		handler(bf, func);
 	}
 }
