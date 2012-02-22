515fee5aed12fa169afefe9d0c437bb6a1329272
michaelwskwan <michael.kwan08@imperial.ac.uk>
2012-02-22 02:29:11 +0000

    Added Doxygen comments.

--- bf_insn.h
+++ bf_insn.h
@@ -1,3 +1,11 @@
+/**
+ * \file bf_insn.h
+ * \brief Definition and API of bf_insn.
+ * \details bf_insn objects are <b>libind</b>'s abstraction of machine
+ * instructions.
+ * \author Mike Kwan <michael.kwan08@imperial.ac.uk>
+ */
+
 #ifndef BF_INSN_H
 #define BF_INSN_H
 
@@ -9,82 +17,142 @@ extern "C" {
 #include "bf_basic_blk.h"
 #include "include/htable.h"
 
+/**
+ * \internal
+ * \struct bf_insn_part
+ * \brief The constituent unit composing a bf_insn.
+ * \details A bf_insn_part represents a list of strings composing the bf_insn.
+ * e.g. push %rbp consists of <i>push</i> and <i>%rbp</i>
+ */
 struct bf_insn_part {
 	struct list_head list;
 	char *		 str;
 };
 
-/*
- * Our abstraction of an instruction. A bf_insn consists of a list of parts
- * which hold the mnemonic and operands.
+/**
+ * \struct bf_insn
+ * \brief <b>libind</b>'s abstraction of an instruction.
+ * \details A bf_insn consists of a list of its constituent parts
+ * (bf_insn_part objects).
  */
 struct bf_insn {
-	/*
-	 * Holds the address of the instruction.
+	/**
+	 * \var vma
+	 * \brief The VMA of the instruction.
 	 */
 	bfd_vma		      vma;
 
-	/*
-	 * Start of linked list of parts.
+	/**
+	 * \internal
+	 * \var part_list
+	 * \brief Start of linked list of parts (bf_insn_part objects).
 	 */
 	struct list_head      part_list;
 
-	/*
-	 * Entry into the hashtable of binary_file.
+	/**
+	 * \internal
+	 * \var entry
+	 * \brief Entry into the binary_file.insn_table hashtable of
+	 * binary_file.
 	 */
 	struct htable_entry   entry;
 
-	/*
-	 * The basic block containing the instruction.
+	/**
+	 * \var bb
+	 * \brief The bf_basic_blk containing the bf_insn.
 	 */
 	struct bf_basic_blk * bb;
 };
 
-/*
- * Returns a bf_insn object. bf_close_insn must be called to allow the object
- * to properly clean up.
+/**
+ * \internal
+ * \brief Creates a new bf_insn object.
+ * \param bb The bf_basic_blk containing the bf_insn.
+ * \param vma The VMA of the bf_insn.
+ * \return A bf_insn object.
+ * \note bf_close_insn must be called to allow the object to properly clean up.
  */
-extern struct bf_insn * bf_init_insn(struct bf_basic_blk *, bfd_vma);
+extern struct bf_insn * bf_init_insn(struct bf_basic_blk * bb, bfd_vma vma);
 
-/*
- * Adds to the tail of the part list.
+/**
+ * \internal
+ * \brief Appends to the tail of the parts list.
+ * \param insn The bf_insn whose parts list is to be appended to.
+ * \param str The string to append.
  */
-extern void bf_add_insn_part(struct bf_insn *, char *);
+extern void bf_add_insn_part(struct bf_insn * insn, char * str);
 
-/*
- * Prints the bf_insn to stdout.
+/**
+ * \brief Prints the bf_insn to stdout.
+ * \param The bf_insn to be printed.
+ * \details This function would generally be used for debug or demonstration
+ * purposes since the format of the output is not easily customisable. The
+ * bf_insn should be manually printed if customised output is desired.
  */
-extern void bf_print_insn(struct bf_insn *);
+extern void bf_print_insn(struct bf_insn * insn);
 
-/*
- * Prints the bf_insn to a FILE.
+/**
+ * \internal
+ * \brief Prints the bf_insn to a FILE in dot format.
+ * \param stream An open FILE to be written to.
+ * \param insn The bf_insn to be written.
  */
-extern void bf_print_insn_dot(FILE *, struct bf_insn *);
+extern void bf_print_insn_dot(FILE * stream, struct bf_insn * insn);
 
-/*
- * Closes a bf_insn obtained from calling bf_init_insn.
+/**
+ * \internal
+ * \brief Closes a bf_insn object.
+ * \param insn The bf_insn to be closed.
  */
-extern void bf_close_insn(struct bf_insn *);
+extern void bf_close_insn(struct bf_insn * insn);
 
-/*
- * Adds a basic block to the insn_table of binary_file.
+/**
+ * \internal
+ * \brief Adds a bf_insn to the binary_file.insn_table.
+ * \param bf The binary_file holding the binary_file.insn_table to be added to.
+ * \param insn The bf_insn to be added.
  */
-extern void bf_add_insn(struct binary_file *, struct bf_insn *);
+extern void bf_add_insn(struct binary_file * bf, struct bf_insn * insn);
 
-/*
- * Gets instruction for the starting VMA.
+/**
+ * \brief Gets the bf_insn object for the starting VMA.
+ * \param bf The binary_file to be searched.
+ * \param vma The VMA of the bf_insn being searched for.
+ * \return The bf_insn starting at vma or NULL if no bf_insn has been
+ * discovered at that address.
  */
-extern struct bf_insn * bf_get_insn(struct binary_file *, bfd_vma);
+extern struct bf_insn * bf_get_insn(struct binary_file * bf, bfd_vma vma);
 
-/*
- * Checks for the existence of an instruction in the insn_table of binary_file.
+/**
+ * \brief Checks whether a discovered bf_insn exists for a VMA.
+ * \param bf The binary_file to be searched.
+ * \param vma The VMA of the bf_insn being searched for.
+ * \return TRUE if a bf_insn could be found, otherwise FALSE.
  */
-extern bool bf_exists_insn(struct binary_file *, bfd_vma);
+extern bool bf_exists_insn(struct binary_file * bf, bfd_vma vma);
 
-/*
- * Releases memory for all instructions currently stored.
+/**
+ * \internal
+ * \brief Releases memory for all currently discovered bf_insn objects.
+ * \param bf The binary_file holding the binary_file.insn_table to be purged.
  */
-extern void bf_close_insn_table(struct binary_file *);
+extern void bf_close_insn_table(struct binary_file * bf);
+
+/**
+ * \brief Invokes a callback for each discovered bf_insn.
+ * \param bf The binary_file holding the bf_insn objects.
+ * \param handler The callback to be invoked for each bf_insn.
+ */
+extern void bf_for_each_insn(struct binary_file * bf,
+		void (*handler)(struct binary_file *, struct bf_insn *));
+
+/**
+ * \brief Invokes a callback for each part in a bf_insn.
+ * \param insn The bf_insn being analysed.
+ * \param handler The callback to be invoked for each bf_insn.
+ */
+extern void bf_for_each_insn_part(struct bf_insn * insn,
+		void (*handler)(struct bf_insn *, char *));
 
 #ifdef __cplusplus
 }
