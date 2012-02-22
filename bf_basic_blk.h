1e354e219d3a1158e90de5d6fe97dc016eecc8de
michaelwskwan <michael.kwan08@imperial.ac.uk>
2012-02-22 02:26:55 +0000

    Added Doxygen comments.

--- bf_basic_blk.h
+++ bf_basic_blk.h
@@ -2,14 +2,14 @@
  * \file bf_basic_blk.h
  * \brief Definition and API of bf_basic_blk.
  * \details bf_basic_blk objects are used to represent the control flow of the
- * target. Each node of a generated control flow graph (CFG) consists of a
+ * target. Each node of a generated control flow graph (<b>CFG</b>) consists of a
  * bf_basic_blk. A CFG can be traversed either:
  *	- Manually through its starting basic block
- * 	- Through visitor callbacks such as bf_for_each_block()
+ * 	- Through visitor callbacks such as bf_for_each_basic_blk()
  *	- Through the CFG navigation APIs provided by bf_cfg.h (for
  * displaying or outputting the CFG)
  *
- * The static analysis performed by libind currently ignores indirect
+ * The static analysis performed by <b>libind</b> currently ignores indirect
  * calls. This means that even if a user disassembles from the entry point,
  * there is a chance that not all code is reachable. Hence, a binary_file can
  * hold multiple unconnected CFGs generated from disassembling at different
@@ -31,18 +31,21 @@ extern "C" {
 
 
 /**
+ * \internal
  * \struct bf_basic_blk_part
  * \brief The constituent unit composing a bf_basic_block.
  * \details A bf_basic_blk_part represents a list of bf_insn objects.
  */
 struct bf_basic_blk_part {
 	/**
+	 * \internal
 	 * \var list
 	 * \brief The list node which is linked onto bf_basic_blk.part_list.
 	 */
 	struct list_head list;
 
 	/**
+	 * \internal
 	 * \var insn
 	 * \brief The bf_insn being encapsulated by this bf_basic_blk_part.
 	 */
@@ -51,7 +54,7 @@ struct bf_basic_blk_part {
 
 /**
  * \struct bf_basic_blk
- * \brief libind's abstraction of a basic block.
+ * \brief <b>libind</b>'s abstraction of a basic block.
  * \details A bf_basic_blk consists of a list of its constituent instructions
  * (bf_insn objects).
  */
@@ -63,12 +66,14 @@ struct bf_basic_blk {
 	bfd_vma		      vma;
 
 	/**
+	 * \internal
 	 * \var part_list
 	 * \brief Start of linked list of parts (instructions/bf_insn objects).
 	 */
 	struct list_head      part_list;
 
 	/**
+	 * \internal
 	 * \var entry
 	 * \brief Entry into the binary_file.bb_table hashtable of binary_file.
 	 */
@@ -98,12 +103,13 @@ struct bf_basic_blk {
 
 	/**
 	 * \var sym
-	 * \brief A bf_sym associated bf_basic_blk.vma.
+	 * \brief A bf_sym associated with bf_basic_blk.vma.
 	 */
 	struct bf_sym *	      sym;
 };
 
 /**
+ * \internal
  * \brief Creates a new bf_basic_blk object.
  * \param bf The binary_file being analysed.
  * \param vma The starting address of the basic block.
@@ -115,6 +121,7 @@ extern struct bf_basic_blk * bf_init_basic_blk(struct binary_file * bf,
 		bfd_vma vma);
 
 /**
+ * \internal
  * \brief Splits a bf_basic_blk at the specified VMA.
  * \param bf The binary_file being analysed.
  * \param bb The bf_basic_blk to be split.
@@ -125,6 +132,7 @@ extern struct bf_basic_blk * bf_split_blk(struct binary_file * bf,
 		struct bf_basic_blk * bb, bfd_vma vma);
 
 /**
+ * \internal
  * \brief Creates a link between two bf_basic_blk objects.
  * \param bb The bf_basic_blk the link starts from.
  * \param bb2 The bf_basic_blk the link goes to.
@@ -133,6 +141,7 @@ extern void bf_add_next_basic_blk(struct bf_basic_blk * bb,
 		struct bf_basic_blk * bb2);
 
 /**
+ * \internal
  * \brief Appends to the tail of the instruction list.
  * \param bb The bf_basic_blk whose instruction list is to be appended to.
  * \param insn The instruction to append.
@@ -150,13 +159,15 @@ extern void bf_add_insn_to_bb(struct bf_basic_blk * bb,
 extern void bf_print_basic_blk(struct bf_basic_blk * bb);
 
 /**
+ * \internal
  * \brief Prints the bf_basic_blk to a FILE in dot format.
- * \param An open FILE to be written to.
- * \param The bf_basic_blk to be written.
+ * \param stream An open FILE to be written to.
+ * \param bb The bf_basic_blk to be written.
  */
 extern void bf_print_basic_blk_dot(FILE * stream, struct bf_basic_blk * bb);
 
 /**
+ * \internal
  * \brief Closes a bf_basic_blk object.
  * \param bb The bf_basic_blk to be closed.
  * \note This will not call bf_close_insn for each bf_insn contained in the
@@ -165,16 +176,16 @@ extern void bf_print_basic_blk_dot(FILE * stream, struct bf_basic_blk * bb);
 extern void bf_close_basic_blk(struct bf_basic_blk * bb);
 
 /**
+ * \internal
  * \brief Adds a bf_basic_blk to the binary_file.bb_table.
  * \param bf The binary_file holding the binary_file.bb_table to be added to.
- * added to.
  * \param bb The bf_basic_blk to be added.
  */
 extern void bf_add_bb(struct binary_file * bf, struct bf_basic_blk * bb);
 
 /**
  * \brief Gets the bf_basic_blk object for the starting VMA.
- * \param bf The binary_file holding the binary_file.bb_table to be searched.
+ * \param bf The binary_file to be searched.
  * \param vma The VMA of the bf_basic_blk being searched for.
  * \return The bf_basic_blk starting at vma or NULL if no bf_basic_blk has been
  * discovered at that address.
@@ -183,25 +194,34 @@ extern struct bf_basic_blk * bf_get_bb(struct binary_file * bf, bfd_vma vma);
 
 /**
  * \brief Checks whether a discovered bf_basic_blk exists for a VMA.
- * \param bf The binary_file holding the binary_file.bb_table to be searched.
+ * \param bf The binary_file to be searched.
  * \param vma The VMA of the bf_basic_blk being searched for.
  * \return TRUE if a bf_basic_blk could be found, otherwise FALSE.
  */
 extern bool bf_exists_bb(struct binary_file * bf, bfd_vma vma);
 
 /**
- * \brief Releases memory for all currently discovered basic blocks.
+ * \internal
+ * \brief Releases memory for all currently discovered bf_basic_blk objects.
  * \param bf The binary_file holding the binary_file.bb_table to be purged.
  */
 extern void bf_close_bb_table(struct binary_file * bf);
 
 /**
  * \brief Invokes a callback for each discovered bf_basic_blk.
- * \param bf The binary_file holding the binary_file.bb_table to be enumerated.
+ * \param bf The binary_file holding the bf_basic_blk objects.
  * \param handler The callback to be invoked for each bf_basic_blk.
  */
 extern void bf_for_each_basic_blk(struct binary_file * bf,
 		void (*handler)(struct binary_file *, struct bf_basic_blk *));
+
+/**
+ * \brief Invokes a callback for each bf_insn in a bf_basic_blk.
+ * \param bb The bf_basic_blk being analysed.
+ * \param handler The callback to be invoked for each bf_insn.
+ */
+extern void bf_for_each_basic_blk_insn(struct bf_basic_blk * bb,
+		void (*handler)(struct bf_basic_blk *, struct bf_insn *));
 
 #ifdef __cplusplus
 }
