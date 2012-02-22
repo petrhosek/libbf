27ee736a361e73558cbbfd802fcee026baaf0601
michaelwskwan <michael.kwan08@imperial.ac.uk>
2012-02-22 00:10:01 +0000

    Added Doxygen comments

--- bf_cfg.h
+++ bf_cfg.h
@@ -1,3 +1,13 @@
+/**
+ * \file bf_cfg.h
+ * \brief API of bf_cfg.
+ * \details bf_cfg contains functions designed for dumping an entire CFG
+ * conveniently. These functions are useful for general debugging and
+ * demonstration. If a more customised output is desired, the CFG can be
+ * manually traversed through its starting bf_basic_blk.
+ * \author Mike Kwan <michael.kwan08@imperial.ac.uk>
+ */
+
 #ifndef BF_CFG_H
 #define BF_CFG_H
 
@@ -7,15 +17,18 @@ extern "C" {
 
 #include "bf_basic_blk.h"
 
-/*
- * Prints the CFG starting from the basic block to stdout.
+/**
+ * \brief Prints the CFG to stdout.
+ * \param bb The bf_basic_blk of the root of the CFG.
  */
-extern void print_cfg_stdout(struct bf_basic_blk *);
+extern void print_cfg_stdout(struct bf_basic_blk * bb);
 
-/*
- * Prints a DOT file representing the CFG starting from the basic block.
+/**
+ * \brief Prints the CFG as a DOT file.
+ * \param stream An open FILE to be written to.
+ * \param bb The bf_basic_blk of the root of the CFG.
  */
-extern void print_cfg_dot(FILE *, struct bf_basic_blk *);
+extern void print_cfg_dot(FILE * stream, struct bf_basic_blk * bb);
 
 #ifdef __cplusplus
 }
