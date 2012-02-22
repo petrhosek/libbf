ebb63f0cd23fc400b962f3fd3b9196fe6e63ec3a
michaelwskwan <michael.kwan08@imperial.ac.uk>
2012-02-22 00:35:12 +0000

    Added Doxygen comments. Consider adding this file to Doxygen ignore list since its functions are for internal use.

--- bf_disasm.h
+++ bf_disasm.h
@@ -1,3 +1,18 @@
+/**
+ * \internal
+ * \file bf_disasm.h
+ * \brief API of bf_disasm.
+ * \details bf_disasm is responsible for wrapping the libopcodes disassembler
+ * and generating useful CFGs. bf_disasm abstracts the callbacks required by
+ * libopcodes as well as the internal disassembling functions of libind.
+ *
+ * The functions provided should not be used by users of libind. They are
+ * internal functions exposed for convenience. disassemble_binary_file_entry()
+ * and disassemble_binary_file_symbol(), which are thin wrappers around
+ * bf_disasm functions, should be called for the generation of CFGs.
+ * \author Mike Kwan <michael.kwan08@imperial.ac.uk>
+ */
+
 #ifndef BF_DISASM_H
 #define BF_DISASM_H
 
@@ -8,26 +23,22 @@ extern "C" {
 #include "binary_file.h"
 #include <ctype.h>
 
-/*
- * This file contains libopcodes callbacks and internal disassembling functions
- */
-
-/*
- * Provides the callback as part of libopcodes initialisation. Will eventually
- * be renamed.
+/**
+ * \brief This is an internal callback invoked by the libopcodes disassembler.
+ * \warning This callback should only be used internally.
  */
 extern int binary_file_fprintf(void *, const char *, ...);
 
-/*
- * Disassemble from the passed in VMA and construct a CFG. The bool specifies
- * whether the symbol should be treated as a function.
+/**
+ * \brief Generate a CFG from a specified root.
+ * \warning This function should only be used internally.
  */
-extern struct bf_basic_blk * disasm_generate_cflow(struct binary_file *,
-		bfd_vma, bool);
+extern struct bf_basic_blk * disasm_generate_cflow(struct binary_file * bf,
+		bfd_vma vma, bool is_func);
 
-/*
- * Disassemble from a symbol. The bool specifies whether the symbol should be
- * treated as a function.
+/**
+ * \brief Generate a CFG using the address of the symbol as the root.
+ * \warning This function should only be used internally.
  */
 extern struct bf_basic_blk * disasm_from_sym(struct binary_file *, asymbol *,
 		bool);
