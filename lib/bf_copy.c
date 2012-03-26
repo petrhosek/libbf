cbf2aa328e856377206569449b373afb9b4a6a20
michaelwskwan <michael.kwan08@imperial.ac.uk>
2012-03-26 15:37:59 +0100

    Wrote test code which simply uses objcopy to perform a copy.

--- lib/bf_copy.c
+++ lib/bf_copy.c
@@ -1,6 +1,23 @@
 #include "bf_copy.h"
 
-bfd * bf_create_writable_bfd(bfd * abfd, char * output_path)
+bfd * bf_create_writable_bfd(char * input_path, bfd * abfd, char * output_path)
 {
-	return NULL;
+	/* Copying with objcopy first. This is just to check our test works. */
+
+	char * cmd  = "objcopy ";
+	char * cmd2 = " ";
+	char copy_cmd[strlen(cmd) + strlen(input_path) +
+		strlen(cmd2) + strlen(output_path) + 1];
+
+	strcpy(copy_cmd, cmd);
+	strcat(copy_cmd, input_path);
+	strcat(copy_cmd, cmd2);
+	strcat(copy_cmd, output_path);
+
+	if(system(copy_cmd)) {
+		perror("Problem copying");
+		xexit(-1);
+	}
+
+	return bfd_openr(input_path, NULL);
 }
