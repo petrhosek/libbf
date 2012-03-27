185b0bd0d241d0ad41dbeb547853e6410281ecbd
michaelwskwan <michael.kwan08@imperial.ac.uk>
2012-03-27 18:03:23 +0100

    Added trampoline block.

--- tests/detour_targets/detour_target.c
+++ tests/detour_targets/detour_target.c
@@ -1,6 +1,65 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#ifdef __x86_64__
+	/* Should find a smarter way of doing this expansion */
+	#define TRAMPOLINE_LENGTH 41
+	#define TRAMPOLINE_BLOCK \
+		({ \
+		    /*
+		     * Our detour takes up 14 bytes. Worst case is if there is
+		     * an instruction on the 14th byte. The maximum length of
+		     * an instruction is 15 bytes so we have to pad 27 bytes to
+		     * be safe. Then we add another 14 bytes for the actual
+		     * trampoline. This means the total padding required is 41
+		     * bytes.\
+		     */ \
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		});
+#else
+	#define TRAMPOLINE_BLOCK
+#endif
+
 /*
  * func1 is invoked by the regular execution of detour_test.
  */
@@ -16,6 +75,8 @@ void func1(void)
 void func2(void)
 {
 	puts("func2 was invoked");
+
+	TRAMPOLINE_BLOCK
 }
 
 int main(void)
