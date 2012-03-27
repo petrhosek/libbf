1035455abc933f1e942e3a8e7452a5fd8eb03e96
michaelwskwan <michael.kwan08@imperial.ac.uk>
2012-03-27 18:03:00 +0100

    Refactored code for detouring. Added lots of comments since the detouring is non-trivial. Started to add trampolining.

--- lib/bf_detour.c
+++ lib/bf_detour.c
@@ -1,15 +1,22 @@
 #include "bf_detour.h"
 
 /*
- * Returns the offset into the bf_basic_blk of the next whole instruction
- * from the end of the patched code.
+ * This function takes a bf_basic_blk that has been detoured or will be.
+ * It considers the bytes directly after the end of the detour. It returns
+ * the offset of the next whole instruction. This offset is from the start of
+ * the section containing the bf_basic_blk. It is assumed that the size of the
+ * bf_basic_blk is larger than BF_DETOUR_LENGTH32 and BF_DETOUR_LENGTH64 for
+ * 32 and 64 bit targets respectively.
  */
-static int get_offset_next_insn(struct binary_file * bf,
+static int get_offset_insn_after_detour(struct binary_file * bf,
 		struct bf_basic_blk * bb)
 {
 	int bb_size = bf_get_bb_size(bf, bb);
 	int i;
 
+	/*
+	 * From the end of the current basic block, find the next instruction.
+	 */
 	for(i = bf->bitiness == arch_32
 			? BF_DETOUR_LENGTH32 : BF_DETOUR_LENGTH64;
 			i < bb_size; i++) {
@@ -22,92 +29,215 @@ static int get_offset_next_insn(struct binary_file * bf,
 }
 
 /*
- * After the detour, we pad up to the next instruction with NOP. This is not
- * necessary but it helps improve how the disassembly looks afterwards.
+ * This function takes in a bf_basic_blk that has been detoured or will be.
+ * It considers the bytes directly after the end of the detour. If these bytes
+ * represent the end of an instruction which was partially overwritten, they
+ * are replaced by NOP up to the next whole instruction.
  */
 static void pad_till_next_insn(struct binary_file * bf,
-		struct bf_mem_block * sec, struct bf_basic_blk * bb)
+		asection * sec, struct bf_basic_blk * bb)
 {
 	int bb_size = bf_get_bb_size(bf, bb);
 
 	if(bf->bitiness == arch_32) {
 	} else {
-		if(bb_size > BF_DETOUR_LENGTH64 ||
-				!bf_exists_insn(bf, bb->vma +
-				BF_DETOUR_LENGTH64)) {
-			asection * asec = sec->section;
-			bfd_byte   buf[asec->size];
-			int i;
+		/* This case should not really be hit. */
+		if(bb_size > BF_DETOUR_LENGTH64) {
+			return;
+		/*
+		 * If the byte directly after the detour is already the start
+		 * of an instruction, there is no need to pad at all!
+		 */
+		} else if(bf_exists_insn(bf, bb->vma + BF_DETOUR_LENGTH64)) {
+			return;
+		} else {
+			bfd_byte   buf[sec->size];
+			int 	   i;
+			int 	   offset_next_insn =
+					get_offset_insn_after_detour(bf, bb);
 
-			int offset_next_insn = get_offset_next_insn(bf, bb);
+			bfd_get_section_contents(bf->obfd, sec, buf,
+					0, sec->size);
 
-			bfd_get_section_contents(bf->obfd, asec, buf,
-					0, asec->size);
-
-			for(i = BF_DETOUR_LENGTH64; i < offset_next_insn; i++) {
-				buf[bb->vma - sec->buffer_vma + i] =
-						0x90;				
+			for(i = BF_DETOUR_LENGTH64; i < offset_next_insn;
+					i++) {
+				buf[bb->vma - sec->vma + i] = 0x90;
 			}
 
-			bfd_set_section_contents(bf->obfd, asec, buf,
-					0, asec-> size);
+			bfd_set_section_contents(bf->obfd, sec, buf,
+					0, sec->size);
 		}
 	}
 }
 
 /*
- * TODO: Add proper comments once the 32 bit part is written.
+ * Places a 64 bit detour from 'from' to `to`. This function also checks
+ * whether an instruction has been partially overwritten by the detour. If yes,
+ * the rest of the instruction is padded with NOP. This helps readability of
+ * the final disassembly.
  */
-void bf_detour_func(struct binary_file * bf, struct bf_func * src_func,
+bool bf_detour64(struct binary_file * bf, bfd_vma from, bfd_vma to)
+{
+	asection * sec = load_section_for_vma(bf, from)->section;
+	int i;
+
+	/*
+	 * Load the section in which the detour will be written to into memory.
+	 */
+	bfd_byte buf[sec->size];
+	bfd_get_section_contents(bf->obfd, sec, buf, 0, sec->size);
+
+	/*
+	 * The 64 bit detour works by injecting the following instructions:
+	 *  - PUSH <Low DWORD of absolute destination>
+	 *  - MOVL <High DWORD of absolute destination>, 4(rsp)
+	 *  - RET
+	 *
+	 * This pushes the absolute QWORD address of destination onto the stack
+	 * and then calls RET which uses it as a return address. This method
+	 * uses 14 bytes and does not trash any registers.
+	 */
+	buf[from - sec->vma] = 0x68;
+
+	for(i = 1; i < 5; i++) {
+		buf[from - sec->vma + i] = ((char *)&to)[i - 1];
+	}
+
+	buf[from - sec->vma + 5] = 0xc7;
+	buf[from - sec->vma + 6] = 0x44;
+	buf[from - sec->vma + 7] = 0x24;
+	buf[from - sec->vma + 8] = 0x04;
+
+	for(i = 9; i < 13; i++) {
+		buf[from - sec->vma + i] = ((char *)&to)[i - 5];
+	}
+
+	buf[from - sec->vma + 13] = 0xc3;
+
+	bfd_set_section_contents(bf->obfd, sec, buf, 0, sec->size);
+	return TRUE;
+}
+
+bool bf_detour_basic_blk(struct binary_file * bf, struct bf_basic_blk * src_bb,
+		struct bf_basic_blk * dest_bb)
+{
+	if(bf->obfd == NULL) {
+		return FALSE;
+	}
+
+	if(bf->bitiness == arch_32) {
+		/*
+		 * Check bf_basic_blk is long enough to be detoured.
+		 */
+		if(bf_get_bb_size(bf, src_bb) < BF_DETOUR_LENGTH32) {
+			return FALSE;
+		} else {
+			// bf_detour32()...
+			return TRUE;
+		}
+	} else {
+		/*
+		 * Check bf_basic_blk is long enough to be detoured.
+		 */
+		if(bf_get_bb_size(bf, src_bb) < BF_DETOUR_LENGTH64) {
+			return FALSE;
+		} else {
+			bool success = bf_detour64(bf, src_bb->vma,
+					dest_bb->vma);
+			pad_till_next_insn(bf,
+					load_section_for_vma(bf,
+					src_bb->vma)->section, src_bb);
+			return success;
+		}
+	}
+}
+
+bool bf_detour_func(struct binary_file * bf, struct bf_func * src_func,
 		struct bf_func * dest_func)
 {
-	struct bf_mem_block * src_sec  =
+	return bf_detour_basic_blk(bf, src_func->bb, dest_func->bb);
+}
+
+/*
+ * Returns the offset into the section of the next trampoline. Returns 0 if not
+ * found.
+ */
+static int get_trampoline_offset(struct binary_file * bf,
+		struct bf_mem_block * sec, bfd_vma vma)
+{
+	void * trampoline        = 0;
+	int    trampoline_offset = 0;
+
+	if(bf->bitiness == arch_32) {
+	} else {
+		char	   trampoline_block[BF_TRAMPOLINE_LENGTH64];
+		asection * asec = sec->section;
+		bfd_byte buf[asec->size];
+
+		memset(trampoline_block, 0x90, BF_TRAMPOLINE_LENGTH64);
+
+		bfd_get_section_contents(bf->obfd, asec, buf,
+				0, asec->size);
+
+		trampoline = memmem(buf + (vma - sec->buffer_vma),
+				asec->size - ((vma - sec->buffer_vma)),
+				trampoline_block, BF_TRAMPOLINE_LENGTH64);
+
+		if(trampoline != NULL) {
+			trampoline_offset = ((bfd_byte *)trampoline) - buf;
+		}
+	}
+
+	return trampoline_offset;
+}
+
+bool bf_detour_basic_blk_with_trampoline(struct binary_file * bf,
+		struct bf_basic_blk * src_bb, struct bf_basic_blk * dest_bb)
+{
+	return TRUE;
+}
+
+bool bf_detour_func_with_trampoline(struct binary_file * bf,
+		struct bf_func * src_func, struct bf_func * dest_func)
+{
+	struct bf_mem_block * src_sec =
 			load_section_for_vma(bf, src_func->vma);
 	struct bf_mem_block * dest_sec =
 			load_section_for_vma(bf, dest_func->vma);
 
 	if(bf->obfd == NULL) {
-		return;
+		return FALSE;
 	}
 
 	if(bf->bitiness == arch_32) {
-		perror("Detouring not implemented for 32 bit yet");
+		perror("Trampolining not implemented for 32 bit yet");
 	} else {
-		if(bf_get_bb_size(bf, src_func->bb) < BF_DETOUR_LENGTH64) {
-			perror("The basic block of the source basic block "\
-					"is too small to detour.");
+		int trampoline_offset = get_trampoline_offset(bf, dest_sec,
+				dest_func->vma);
+
+		if(trampoline_offset == 0) {
+			printf("Failed to locate trampoline after performing "\
+					"a forward linear sweep from 0x%lX",
+					dest_func->vma);
 		} else {
-			asection * asec   = src_sec->section;
-			bfd_byte   buf[asec->size];
-			int i;
-			uint64_t   target = dest_func->vma;
+			int offset_next_insn =
+					get_offset_insn_after_detour(bf, src_func->bb);
+			bfd_byte src_buf[src_sec->section->size];
+			bfd_byte dest_buf[dest_sec->section->size];
 
-			bfd_get_section_contents(bf->obfd, asec, buf,
-					0, asec->size);
+			bfd_get_section_contents(bf->obfd, src_sec->section,
+					src_buf, 0, src_sec->section->size);
+			bfd_get_section_contents(bf->obfd, dest_sec->section,
+					dest_buf, 0, dest_sec->section->size);
 
-			buf[src_func->vma - src_sec->buffer_vma] = 0x68;
+			memcpy(dest_buf + trampoline_offset, src_buf +
+					(src_func->vma - src_sec->buffer_vma),
+					offset_next_insn);
 
-			for(i = 1; i < 5; i++) {
-				buf[src_func->vma - src_sec->buffer_vma + i] =
-						((char *)&target)[i - 1];
-			}
+			bfd_set_section_contents(bf->obfd, dest_sec->section,
+					dest_buf, 0, dest_sec->section->size);
 
-			buf[src_func->vma - src_sec->buffer_vma + 5] = 0xc7;
-			buf[src_func->vma - src_sec->buffer_vma + 6] = 0x44;
-			buf[src_func->vma - src_sec->buffer_vma + 7] = 0x24;
-			buf[src_func->vma - src_sec->buffer_vma + 8] = 0x04;
-
-			for(i = 9; i < 13; i++) {
-				buf[src_func->vma - src_sec->buffer_vma + i] =
-						((char *)&target)[i - 5];
-			}
-
-			buf[src_func->vma - src_sec->buffer_vma + 13] = 0xc3;
-
-			bfd_set_section_contents(bf->obfd, asec, buf,
-					0, asec-> size);
-
-			pad_till_next_insn(bf, src_sec, src_func->bb);
+			
 		}
 	}
 }
