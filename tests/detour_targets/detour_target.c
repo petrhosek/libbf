f1939350226873f0e2a498df5754be35f527ad2b
Mike Kwan <michael.kwan08@imperial.ac.uk>
2012-05-20 22:17:13 +0100

    Added trampoline block for 32 bit target.

--- tests/detour_targets/detour_target.c
+++ tests/detour_targets/detour_target.c
@@ -3,15 +3,15 @@
 
 #ifdef __x86_64__
 	/* Should find a smarter way of doing this expansion */
-	#define TRAMPOLINE_LENGTH 41
+	#define TRAMPOLINE_LENGTH 42
 	#define TRAMPOLINE_BLOCK \
 		({ \
 		    /*
 		     * Our detour takes up 14 bytes. Worst case is if there is
 		     * an instruction on the 14th byte. The maximum length of
-		     * an instruction is 15 bytes so we have to pad 27 bytes to
+		     * an instruction is 15 bytes so we have to pad 28 bytes to
 		     * be safe. Then we add another 14 bytes for the actual
-		     * trampoline. This means the total padding required is 41
+		     * trampoline. This means the total padding required is 42
 		     * bytes.\
 		     */ \
 		    asm volatile ("nop\n");\
@@ -55,9 +55,45 @@
 		    asm volatile ("nop\n");\
 		    asm volatile ("nop\n");\
 		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
 		});
 #else
-	#define TRAMPOLINE_BLOCK
+	#define TRAMPOLINE_LENGTH 24
+	#define TRAMPOLINE_BLOCK \
+		({ \
+		    /*
+		     * Our detour takes up 5 bytes. Worst case is if there is
+		     * an instruction on the 5th byte. The maximum length of
+		     * an instruction is 15 bytes so we have to pad 19 bytes to
+		     * be safe. Then we add another 5 bytes for the actual
+		     * trampoline. This means the total padding required is 24
+		     * bytes.\
+		     */ \
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		});
 #endif
 
 /*
