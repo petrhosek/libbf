cacf96613252d7c0af501b472182ac0cc4a6030e
michaelwskwan <michael.kwan08@imperial.ac.uk>
2012-05-25 18:37:30 +0100

    Added trampoline.

--- evaluation/arguments/arguments.c
+++ evaluation/arguments/arguments.c
@@ -2,15 +2,21 @@
 #include <libbind/binary_file.h>
 #include <libbind/detour.h>
 
-int main(void)
+void trampoline_target(char * target, char * output)
 {
-	struct bin_file * bf = load_bin_file("target64", "target64-output");
+	struct bin_file * bf = load_bin_file(target, output);
 
 	bf_trampoline_basic_blk(bf,
 			disasm_bin_file_sym(bf,
 			symbol_find(&bf->sym_table, "f1"), TRUE),
 			disasm_bin_file_sym(bf,
-			symbol_find(&bf->sym_table, "f_detour"), TRUE));
+			symbol_find(&bf->sym_table, "f_trampoline"), TRUE));
 	close_bin_file(bf);
+}
+
+int main(void)
+{
+	trampoline_target("target64", "target64-output");
+	trampoline_target("target32", "target32-output");
 	return EXIT_SUCCESS;
 }
--- evaluation/arguments/target.c
+++ evaluation/arguments/target.c
@@ -1,25 +1,125 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdint.h>
 
-void f_detour(void)
+#ifdef __x86_64__
+	/* Should find a smarter way of doing this expansion */
+	#define TRAMPOLINE_LENGTH 43
+	#define TRAMPOLINE_BLOCK \
+		({ \
+		    /*
+		     * Our detour takes up 14 bytes. Worst case is if there is
+		     * an instruction on the 14th byte. The maximum length of
+		     * an instruction is 15 bytes so we have to pad 28 bytes to
+		     * be safe. Then we add another 14 bytes for the actual
+		     * trampoline. One extra byte is needed to clean up the
+		     * stack. This means the total padding required is 43
+		     * bytes.\
+		     */ \
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		});
+#else
+	#define TRAMPOLINE_LENGTH 25
+	#define TRAMPOLINE_BLOCK \
+		({ \
+		    /*
+		     * Our detour takes up 5 bytes. Worst case is if there is
+		     * an instruction on the 5th byte. The maximum length of
+		     * an instruction is 15 bytes so we have to pad 19 bytes to
+		     * be safe. Then we add another 5 bytes for the actual
+		     * trampoline. One extra byte is needed to clean up the
+		     * stack. This means the total padding required is 25
+		     * bytes.
+		     */ \
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		    asm volatile ("nop\n");\
+		});
+#endif
+
+void f_trampoline(void)
 {
 	#ifdef __x86_64__
-		uint8_t * rsp;
+		void * rsp;
 		asm ("movq %%rsp, %0" : "=g" (rsp));
 
 		printf("%p\n", rsp);
 	#else
-		uint32_t i;
+		void * i;
 		asm("movl %%esp, %0" : "=g" (i));
 
-		printf("%X\n", i);
+		printf("%p\n", i);
 	#endif
+
+	TRAMPOLINE_BLOCK
 }
 
 void f1(int i, double j, char * k)
 {
-//	f_detour();
+//	f_trampoline();
 	printf("%p\n", &i);
 /*	printf("i = %d\n", i);
 	printf("j = %g\n", j);
