d3cc28510e33b1e0d556fb25be16ad9dd16553b3
michaelwskwan <michael.kwan08@imperial.ac.uk>
2012-05-26 20:26:44 +0100

    Renamed structure basic_blk to bf_basic_blk to prevent namespace pollution. Fixed some bugs where bf_for_each_insn was being used instead of bf_for_each_basic_blk_insn. Got rid of superfluous arguments which are not needed.

--- lib/basic_blk.c
+++ lib/basic_blk.c
@@ -1,24 +1,25 @@
 #include "insn.h"
 #include "basic_blk.h"
 
-struct basic_blk * bf_init_basic_blk(struct bin_file * bf, bfd_vma vma)
+struct bf_basic_blk * bf_init_basic_blk(struct bin_file * bf, bfd_vma vma)
 {
-	struct basic_blk * bb = xmalloc(sizeof(struct basic_blk));
-	bb->vma		      = vma;
-	bb->target	      = NULL;
-	bb->target2	      = NULL;
-	bb->sym		      = rb_search_symbol(&bf->sym_table, (void *)vma);
+	struct bf_basic_blk * bb = xmalloc(sizeof(struct bf_basic_blk));
+	bb->vma			 = vma;
+	bb->target		 = NULL;
+	bb->target2		 = NULL;
+	bb->sym			 = rb_search_symbol(&bf->sym_table,
+			(void *)vma);
 
 	INIT_LIST_HEAD(&bb->part_list);
 	return bb;
 }
 
-struct basic_blk * bf_split_blk(struct bin_file * bf, struct basic_blk * bb,
-		bfd_vma vma)
+struct bf_basic_blk * bf_split_blk(struct bin_file * bf,
+		struct bf_basic_blk * bb, bfd_vma vma)
 {
-	struct basic_blk *	bb_new = bf_init_basic_blk(bf, vma);
-	struct basic_blk_part * pos;
-	struct basic_blk_part * n;
+	struct bf_basic_blk *	bb_new = bf_init_basic_blk(bf, vma);
+	struct bf_basic_blk_part * pos;
+	struct bf_basic_blk_part * n;
 	
 	list_for_each_entry_safe(pos, n, &bb->part_list, list) {
 		struct bf_insn * insn = pos->insn;
@@ -32,7 +33,7 @@ struct basic_blk * bf_split_blk(struct bin_file * bf, struct basic_blk * bb,
 	return bb_new;
 }
 
-void bf_add_next_basic_blk(struct basic_blk * bb, struct basic_blk * bb2)
+void bf_add_next_basic_blk(struct bf_basic_blk * bb, struct bf_basic_blk * bb2)
 {
 	assert(bb && bb2 && (!bb->target || !bb->target2));
 
@@ -43,20 +44,20 @@ void bf_add_next_basic_blk(struct basic_blk * bb, struct basic_blk * bb2)
 	}
 }
 
-void bf_add_insn_to_bb(struct basic_blk * bb, struct bf_insn * insn)
+void bf_add_insn_to_bb(struct bf_basic_blk * bb, struct bf_insn * insn)
 {
-	struct basic_blk_part * part =
-			xmalloc(sizeof(struct basic_blk_part));
+	struct bf_basic_blk_part * part =
+			xmalloc(sizeof(struct bf_basic_blk_part));
 	part->insn		     = insn;
 
 	INIT_LIST_HEAD(&part->list);
 	list_add_tail(&part->list, &bb->part_list);
 }
 
-void bf_print_basic_blk(struct basic_blk * bb)
+void bf_print_basic_blk(struct bf_basic_blk * bb)
 {
 	if(bb != NULL) {
-		struct basic_blk_part * pos;
+		struct bf_basic_blk_part * pos;
 
 		list_for_each_entry(pos, &bb->part_list, list) {
 			printf("\t");
@@ -66,10 +67,10 @@ void bf_print_basic_blk(struct basic_blk * bb)
 	}
 }
 
-void bf_print_basic_blk_dot(FILE * stream, struct basic_blk * bb)
+void bf_print_basic_blk_dot(FILE * stream, struct bf_basic_blk * bb)
 {
 	if(bb != NULL) {
-		struct basic_blk_part * pos;
+		struct bf_basic_blk_part * pos;
 
 		list_for_each_entry(pos, &bb->part_list, list) {
 			bf_print_insn_dot(stream, pos->insn);
@@ -77,11 +78,11 @@ void bf_print_basic_blk_dot(FILE * stream, struct basic_blk * bb)
 	}
 }
 
-void bf_close_basic_blk(struct basic_blk * bb)
+void bf_close_basic_blk(struct bf_basic_blk * bb)
 {
 	if(bb != NULL) {
-		struct basic_blk_part * pos;
-		struct basic_blk_part * n;
+		struct bf_basic_blk_part * pos;
+		struct bf_basic_blk_part * n;
 
 		list_for_each_entry_safe(pos, n, &bb->part_list, list) {
 			list_del(&pos->list);
@@ -92,53 +93,52 @@ void bf_close_basic_blk(struct basic_blk * bb)
 	}
 }
 
-void bf_add_bb(struct bin_file * bf, struct basic_blk * bb)
+void bf_add_bb(struct bin_file * bf, struct bf_basic_blk * bb)
 {
 	assert(!bf_exists_bb(bf, bb->vma));
 
 	htable_add(&bf->bb_table, &bb->entry, &bb->vma, sizeof(bb->vma));
 }
 
-struct basic_blk * bf_get_bb(struct bin_file * bf, bfd_vma vma)
+struct bf_basic_blk * bf_get_bb(struct bin_file * bf, bfd_vma vma)
 {
 	return hash_find_entry(&bf->bb_table, &vma, sizeof(vma),
-			struct basic_blk, entry);
+			struct bf_basic_blk, entry);
 }
 
-unsigned int bf_get_bb_size(struct bin_file * bf, struct basic_blk * bb)
+unsigned int bf_get_bb_size(struct bf_basic_blk * bb)
 {
 	struct bf_insn * insn;
 	unsigned int	 size = 0;
 
-	bf_for_each_insn(insn, bf) {
+	bf_for_each_basic_blk_insn(insn, bb) {
 		size += insn->size;
 	}
 
 	return size;
 }
 
-unsigned int bf_get_bb_length(struct bin_file * bf, struct basic_blk * bb)
+unsigned int bf_get_bb_length(struct bf_basic_blk * bb)
 {
 	struct bf_insn * insn;
 	unsigned int	 length = 0;
 
-	bf_for_each_insn(insn, bf) {
+	bf_for_each_basic_blk_insn(insn, bb) {
 		length++;
 	}
 
 	return length;
 }
 
-struct bf_insn * bf_get_bb_insn(struct bin_file * bf,
-		struct basic_blk * bb, unsigned int index)
+struct bf_insn * bf_get_bb_insn(struct bf_basic_blk * bb, unsigned int index)
 {
-	if(index >= bf_get_bb_size(bf, bb)) {
+	if(index >= bf_get_bb_size(bb)) {
 		return NULL;
 	} else {
 		struct bf_insn * insn;
 		unsigned int	 cur_index = 0;
-		
-		bf_for_each_insn(insn, bf) {
+
+		bf_for_each_basic_blk_insn(insn, bb) {
 			if(cur_index == index) {
 				return insn;
 			}
@@ -159,7 +159,7 @@ void bf_close_bb_table(struct bin_file * bf)
 {
 	struct htable_entry * cur_entry;
 	struct htable_entry * n;
-	struct basic_blk * bb;
+	struct bf_basic_blk * bb;
 
 	htable_for_each_entry_safe(bb, cur_entry, n, &bf->bb_table, entry) {
 		htable_del_entry(&bf->bb_table, cur_entry);
@@ -168,22 +168,22 @@ void bf_close_bb_table(struct bin_file * bf)
 }
 
 void bf_enum_basic_blk(struct bin_file * bf,
-		void (*handler)(struct bin_file *, struct basic_blk *,
+		void (*handler)(struct bin_file *, struct bf_basic_blk *,
 		void * param), void * param)
 {
 	struct htable_entry * cur_entry;
-	struct basic_blk * bb;
+	struct bf_basic_blk * bb;
 
 	htable_for_each_entry(bb, cur_entry, &bf->bb_table, entry) {
 		handler(bf, bb, param);
 	}
 }
 
-void bf_enum_basic_blk_insn(struct basic_blk * bb,
-		void (*handler)(struct basic_blk *, struct bf_insn *,
+void bf_enum_basic_blk_insn(struct bf_basic_blk * bb,
+		void (*handler)(struct bf_basic_blk *, struct bf_insn *,
 		void * param), void * param)
 {
-	struct basic_blk_part * pos;
+	struct bf_basic_blk_part * pos;
 
 	list_for_each_entry(pos, &bb->part_list, list) {
 		handler(bb, pos->insn, param);
