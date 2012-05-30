1c195e4624710cb103e040f92c3edc73725a6906
Mike Kwan <mk08@doc.ic.ac.uk>
2012-05-30 13:30:52 +0100

    Hoisted usage of visited_bbs hashtable since it does not change for a given bf. This prevents re-evaluation of a pair of basic blocks.

--- evaluation/change/coreutils-change.c
+++ evaluation/change/coreutils-change.c
@@ -23,6 +23,7 @@ struct bb_cmp_info {
 
 struct visited_bb {
 	struct bf_basic_blk * bb;
+	struct bf_basic_blk * bb2;
 	struct htable_entry   entry;
 };
 
@@ -116,10 +117,12 @@ void dump_data(char * bin1, char * bin2, char * bitiness,
 	fclose(file);
 }
 
-void add_visited_bb(struct bb_cmp_info * info, struct bf_basic_blk * bb)
+void add_visited_bb(struct bb_cmp_info * info, struct bf_basic_blk * bb,
+		struct bf_basic_blk * bb2)
 {
 	struct visited_bb * v_bb = malloc(sizeof(struct visited_bb));
 	v_bb->bb		 = bb;
+	v_bb->bb2		 = bb2;
 
 	htable_add(&info->visited_bbs, &v_bb->entry, &bb->vma,
 			sizeof(bb->vma));
@@ -129,13 +132,21 @@ void release_visited_info(struct bb_cmp_info * info)
 {
 	struct htable_entry * cur_entry;
 	struct htable_entry * n;
-	struct visited_bb *   bb;
+	struct visited_bb *   v_bb;
 
-	htable_for_each_entry_safe(bb, cur_entry, n, &info->visited_bbs,
+	htable_for_each_entry_safe(v_bb, cur_entry, n, &info->visited_bbs,
 			entry) {
 		htable_del_entry(&info->visited_bbs, cur_entry);
-		free(bb);
+		free(v_bb);
 	}
+}
+
+bool has_visited_bb(struct bb_cmp_info * info, struct bf_basic_blk * bb,
+		struct bf_basic_blk * bb2)
+{
+	struct visited_bb * v_bb = hash_find_entry(&info->visited_bbs,
+			&bb->vma, sizeof(bb->vma), struct visited_bb, entry);
+	return ((v_bb != NULL) && (v_bb->bb2->vma == bb2->vma));
 }
 
 /*
@@ -158,7 +169,7 @@ bool bb_cmp(struct bb_cmp_info * info, struct bf_basic_blk * bb,
 	/*
 	 * Already visited.
 	 */
-	} else if(htable_find(&info->visited_bbs, &bb->vma, sizeof(bb->vma))) {
+	} else if(has_visited_bb(info, bb, bb2)) {
 		return TRUE;
 	} else {
 		unsigned int length = bf_get_bb_length(bb);
@@ -183,7 +194,7 @@ bool bb_cmp(struct bb_cmp_info * info, struct bf_basic_blk * bb,
 		/*
 		 * Update visited bbs and compare the next bbs in the CFG.
 		 */
-		add_visited_bb(info, bb);
+		add_visited_bb(info, bb, bb2);
 		return bb_cmp(info, bb->target, bb2->target) &&
 				bb_cmp(info, bb->target2, bb2->target2);
 	}
@@ -211,6 +222,8 @@ void compare_bins(char * bitiness, char * bin1, char * bin2,
 
 	printf("Comparing \n%s and \n%s:\n", bin1, bin2);
 
+	htable_init(&info.visited_bbs);
+
 	for_each_symbol(sym, &bf->sym_table) {
 		if(is_func(sym)) {
 			sym2 = symbol_find(&bf2->sym_table, sym->name);
@@ -220,20 +233,18 @@ void compare_bins(char * bitiness, char * bin1, char * bin2,
 			} else {
 				bb1 = disasm_bin_file_sym(bf, sym, TRUE);
 				bb2 = disasm_bin_file_sym(bf2, sym2, TRUE);
-
-				htable_init(&info.visited_bbs);
 				
 				if(!bb_cmp(&info, bb1, bb2)) {
 					ci.same++;
  				} else {
 					ci.modified++;
 				}
-
-				release_visited_info(&info);
-				htable_destroy(&info.visited_bbs);
 			}
 		}
 	}
+
+	release_visited_info(&info);
+	htable_destroy(&info.visited_bbs);
 
 	for_each_symbol(sym, &bf2->sym_table) {
 		if(is_func(sym)) {
