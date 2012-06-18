dadf555231767439441941f77e46f17851df2397
michaelwskwan <michael.kwan08@imperial.ac.uk>
2012-06-18 21:26:00 +0100

    Changed variable names.

--- evaluation/log_rand/log_rand.c
+++ evaluation/log_rand/log_rand.c
@@ -11,15 +11,15 @@ int main(void)
 	 * This means the target is edited as opposed to a new output created.
 	 */
 	struct bin_file * bf = load_bin_file("rand", NULL);
-	struct bf_func *  src_func, * dest_func, * new_func1;
+	struct bf_func *  src_func1, * new_func1, * log_func1;
 
 	disasm_all_func_sym(bf);
-	src_func  = bf_get_func_from_name(bf, "func1");
-	dest_func = bf_get_func_from_name(bf, "log_func");
+	src_func1 = bf_get_func_from_name(bf, "func1");
 	new_func1 = bf_get_func_from_name(bf, "new_func1");
+	log_func1 = bf_get_func_from_name(bf, "log_func");
 
-	bf_detour_func(bf, src_func, new_func1);
-	bf_trampoline_func(bf, new_func1, dest_func);
+	bf_detour_func(bf, src_func1, new_func1);
+	bf_trampoline_func(bf, new_func1, log_func1);
 
 	close_bin_file(bf);
 	return EXIT_SUCCESS;
